acb1f754f2aca0a80e0f3d9de68a3aa8db313825
ShareMan <sharemant@163.com>
2019-02-06 22:24:39 +0800

    1,[Fix] Fix the problem of multi editormd instances in one page.

--- src/Editor.php
+++ src/Editor.php
@@ -20,31 +20,31 @@ class Editor extends Field
         $valueType = config('admin.extensions.editormd.valueType');
         if (config('admin.extensions.editormd.dynamicMode')) {
             $this->script = <<<EOT
-        var editorMd;
+        var editorMd{$this->id};
         $(document).ready(function(){
-            $("#editormd-create-btn").click(function(){
-                $("#editormd-create-btn").hide();
-                    var valueType = '{$valueType}';
-                    var config = Object.assign({id:'{$this->id}'}, {$config});
-                    editorMd = editormd(config);
-                    // Fix editormd V1.5.0 bug (Previewing close button default set to show when loaded).
-                    $(".editormd-preview-close-btn").hide();
-                    // Set the content value type.
-                    if( config['saveHTMLToTextarea'] ) {
-                       $(".editormd-html-textarea").attr("name", '{$this->id}');
-                    } else {
-                       $(".editormd-markdown-textarea").attr("name", '{$this->id}');
-                    }
-                });           
+            $("#editormd-create-btn-{$this->id}").click(function(){
+                $(this).hide();
+                var valueType = '{$valueType}';
+                var config = Object.assign({id:'{$this->id}'}, {$config});
+                editorMd{$this->id} = editormd(config);
+                // Fix editormd V1.5.0 bug (Previewing close button default set to show when loaded).
+                $("#{$this->id}").find(".editormd-preview-close-btn").hide();
+                // Set the content value type.
+                if( config['saveHTMLToTextarea'] ) {
+                   $(".editormd-html-textarea").attr("name", '{$this->id}');
+                } else {
+                   $(".editormd-markdown-textarea").attr("name", '{$this->id}');
+                }
+            });           
         });
 EOT;
         } else {
             $this->script = <<<EOT
-        var editorMd;
+        var editorMd{$this->id};
         var valueType = '{$valueType}';
         var config = Object.assign({id:'{$this->id}'}, {$config});
         $(document).ready(function(){
-            editorMd = editormd(config);
+            editorMd{$this->id} = editormd(config);
             // Fix editormd V1.5.0 bug (Previewing close button default set to show when loaded).
             $(".editormd-preview-close-btn").hide();
             // Set the content value type.
